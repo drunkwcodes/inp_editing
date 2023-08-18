1b9c3ae61201e1f95f40b9219d5b24ab0fb1a76b
Eric <eric@simutech.com.tw>
2023-08-18 14:01:00 +0800

    bw image

--- image.py
+++ image.py
@@ -4,6 +4,7 @@ import cv2
 
 # import pandas as pd
 import matplotlib.pyplot as plt
+from PIL import Image
 
 
 def main():
@@ -14,8 +15,15 @@ def main():
     plt.axis("off")  # 隱藏座標軸
     plt.show()
 
+    # Convert to black white
+    image = Image.open(imagePath)
+    threshold = 220
+    bw_image = image.convert("L").point(lambda p: 255 if p > threshold else 0, "1")
+    bw_path = "bw.png"
+    bw_image.save(bw_path)
+
     # Gray scale
-    imageGray = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
+    imageGray = cv2.imread(bw_path, cv2.IMREAD_GRAYSCALE)
 
     # Resize
     imageResize200x200 = cv2.resize(imageGray, (200, 200))
