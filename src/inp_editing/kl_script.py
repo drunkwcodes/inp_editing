9bf22c2bca146b5a22ed598c9af3cef2d3ce1cbc
Eric <eric@simutech.com.tw>
2023-09-06 17:08:20 +0800

    get_screenshot() WIP

--- src/inp_editing/kl_script.py
+++ src/inp_editing/kl_script.py
@@ -5,13 +5,13 @@ from pathlib import Path
 import pya
 
 gds_path=str(Path(__file__).parent.parent.parent / "PNL.gds")
-output_path=str(Path(__file__).parent.parent.parent / "bw1.png")
+output_path=str(Path(__file__).parent.parent.parent / "bw2.png")
 app = (
     pya.Application.instance()
 )  # TODO: FIXME module 'pya' has no attribute 'Application'
 mw = app.main_window()
 
-mw.load_layout(gds_path, 1)
+mw.load_layout(gds_path, 2)
 # mw.current_layout()
 
 layout_view = mw.current_view()  
@@ -20,4 +20,13 @@ for lyp in layout_view.each_layer():
     lyp.frame_color    = 0
     lyp.dither_pattern = 0
 
-layout_view.save_image(output_path, 2000, 2000)
+    # print(lyp.layer_index)
+
+    # # Set the visibility of the layer (True for visible, False for invisible)
+    # layer_info.visible = False  # Make the layer invisible
+    if lyp.layer_index() == 4:
+        lyp.visible = False
+screenshot = layout_view.get_screenshot()
+screenshot.save(output_path, "PNG", -1)
+
+# layout_view.save_image(output_path, 2000, 2000)
