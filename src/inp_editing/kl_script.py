5021bc2ba6588dce93cd0749aa98950d2054ebb6
Eric <eric@simutech.com.tw>
2023-09-15 16:18:44 +0800

    Add materials.pkl

--- src/inp_editing/kl_script.py
+++ src/inp_editing/kl_script.py
@@ -5,29 +5,27 @@ from pathlib import Path
 import pya
 
 gds_path=str(Path(__file__).parent.parent.parent / "PNL.gds")
-output_path=str(Path(__file__).parent.parent.parent / "bw6.png")
-app = (
-    pya.Application.instance()
-)  # TODO: FIXME module 'pya' has no attribute 'Application'
-mw = app.main_window()
+output_path=str(Path(__file__).parent.parent.parent / "bw9.png")
 
-mw.load_layout(gds_path, 2)
-# mw.current_layout()
+layout_view = pya.LayoutView()
+layout_view.load_layout(gds_path)
+layout_view.max_hier() 
+layout_view.set_config("background-color", "#ffffff")
+layout_view.set_config("grid-show-ruler", "false")
+layout_view.set_config("grid-visible", "false")
 
-layout_view = mw.current_view()  
 for lyp in layout_view.each_layer():
-    lyp.fill_color     = 0
-    lyp.frame_color    = 0
+    lyp.fill_color = 0
+    lyp.frame_color = 0
     lyp.dither_pattern = 0
-
-    # print(lyp.layer_index)
-
-    # # Set the visibility of the layer (True for visible, False for invisible)
-    # layer_info.visible = False  # Make the layer invisible
-    if lyp.layer_index() == 4:
+    if lyp.layer_index() in [4]:
         lyp.visible = False
+
+layout_view.update_content()
+screenshot = layout_view.get_image(1000, 1000)
+screenshot.save(output_path, "PNG", -1)
 # screenshot = layout_view.get_screenshot()
 # screenshot.save(output_path, "PNG", -1)
 # layout_view.zoom_fit()  # not working
-app.process_events()
-layout_view.save_image(output_path, 2000, 2000)
+# app.process_events()
+# layout_view.save_image(output_path, 2000, 2000)
